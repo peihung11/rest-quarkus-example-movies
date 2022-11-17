ddf0ee83ded9d9241204ce741e9389c65dd4ad13
Pei-Ling <pei20141204@gmail.com>
2022-11-17 08:07:52 +0000

    swagger-ui

--- src/main/java/org/acme/MovieResource.java
+++ src/main/java/org/acme/MovieResource.java
@@ -3,7 +3,7 @@ package org.acme;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +18,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
@@ -27,7 +27,7 @@ import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
-import io.vertx.core.cli.annotations.Description;
+
 
 @Path("/movies")
 @Tag(name = "movie Resource", description="Movie REST APIs")
